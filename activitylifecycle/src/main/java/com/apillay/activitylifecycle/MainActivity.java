65ebd3b061af52ed8e1d6ac9a93a7d72840cfe79
Pillay, Anil (Tax&Accounting) <anil.pillay@thomsonreuters.com>
2016-02-05 13:45:58 -0400

    Complete Activity Life Cycle

--- activitylifecycle/src/main/java/com/apillay/activitylifecycle/MainActivity.java
+++ activitylifecycle/src/main/java/com/apillay/activitylifecycle/MainActivity.java
@@ -1,13 +1,52 @@
 package com.apillay.activitylifecycle;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = "ANIL";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "On Create");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "On Restart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "On Resume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "On Start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "On Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "On Destroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "On Pause");
     }
 }
