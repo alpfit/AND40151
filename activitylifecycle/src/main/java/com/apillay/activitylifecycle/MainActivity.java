01f572bc8a1bded9eea54584359cf0fdf730c2a0
Pillay, Anil (Tax&Accounting) <anil.pillay@thomsonreuters.com>
2016-03-01 18:18:56 -0400

    ContactSelection commit

--- activitylifecycle/src/main/java/com/apillay/activitylifecycle/MainActivity.java
+++ activitylifecycle/src/main/java/com/apillay/activitylifecycle/MainActivity.java
@@ -1,19 +1,41 @@
 package com.apillay.activitylifecycle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.webkit.WebView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = "ANIL";
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = (TextView)findViewById(R.id.hello);
+        tv = (TextView)findViewById(R.id.hello);
         tv.setText(R.string.start);
         Log.i(TAG, "On Create");
+
+        responderIntent();
+    }
+
+    private void responderIntent() {
+        Intent intent = getIntent();
+        WebView wv;
+        String mDatos;
+        Uri datos;
+        if (intent != null && intent.getAction().compareTo(Intent.ACTION_VIEW) == 0) {
+            datos = intent.getData();
+            mDatos = datos.toString();
+            Toast.makeText(MainActivity.this, mDatos, Toast.LENGTH_LONG).show();
+            tv.setText(mDatos);
+            wv = (WebView)findViewById(R.id.webView1);
+            wv.loadUrl(mDatos);
+        }
     }
 
     @Override
